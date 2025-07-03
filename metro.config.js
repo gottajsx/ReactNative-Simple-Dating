ace60f625c28246135a2436cdd1a4d7125290060
gottajsx <gottajsx@gmail.com>
2025-07-03 16:15:19 +0200

    Solving react-native-reanimated-carousel import

---
+++ metro.config.js
@@ -0,0 +1,6 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = wrapWithReanimatedMetroConfig(defaultConfig);
